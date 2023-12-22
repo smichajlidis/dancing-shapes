0360de72c76ef46d7ad551632202411966598ba6
smichajlidis <smichajlidis@gmail.com>
2023-12-22 20:40:45 +0100

    create a circle by Circle class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
+//#include "include/Create_shapes.hpp"
+#include "include/Circle.hpp"
 
-int main() {
-    std::cout << "Hello world!" << std::endl;
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Dancing shapes");
+    Circle circle {100.0f};
+    circle.setShape(25.0f, 150.0f, sf::Color::Green);
+
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        circle.draw(window);
+        window.display();
+    }
+
+    return 0;
 }
