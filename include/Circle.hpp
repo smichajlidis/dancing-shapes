8b41587f50c6f6f1d60b3d95488b5074d0fcba0f
smichajlidis <smichajlidis@gmail.com>
2023-12-22 20:43:42 +0100

    Create circle class with basics methods: draw, setShape and one-arg constructor

---
+++ include/Circle.hpp
@@ -0,0 +1,17 @@
+#ifndef _CIRCLE_HPP_
+#define _CIRCLE_HPP_
+#include <SFML/Graphics.hpp>
+#include "Figure.hpp"
+//#include "Create_shapes.hpp"
+
+class Circle: public Figure {
+private:
+    sf::CircleShape shape;
+
+public:
+    virtual void setShape(float pos_x, float pos_y, sf::Color color) override;
+    virtual void draw(sf::RenderWindow& window) const override;
+    Circle(float);
+};
+
+#endif
