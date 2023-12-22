e9e3af277ab4152bb47c74c28e33c1bed9602c3d
smichajlidis <smichajlidis@gmail.com>
2023-12-22 20:42:19 +0100

    Create abstract Class to creating different shapes

---
+++ include/Figure.hpp
@@ -0,0 +1,12 @@
+#ifndef _FIGURE_HPP_
+#define _FIGURE_HPP_
+#include <SFML/Graphics.hpp>
+
+class Figure {
+public:
+    virtual void setShape(float pos_x, float pos_y, sf::Color color) = 0;
+    virtual void draw(sf::RenderWindow& window) const = 0;
+    virtual ~Figure() {};
+};
+
+#endif
