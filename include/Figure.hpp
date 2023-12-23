99d97660ed61f92e33f25774704af95126bf3e5a
smichajlidis <smichajlidis@gmail.com>
2023-12-23 14:04:51 +0100

    add virtual move method

--- include/Figure.hpp
+++ include/Figure.hpp
@@ -6,6 +6,7 @@ class Figure {
 public:
     virtual void setShape(float pos_x, float pos_y, sf::Color color) = 0;
     virtual void draw(sf::RenderWindow& window) const = 0;
+    virtual void move() = 0;
     virtual ~Figure() {};
 };
 
