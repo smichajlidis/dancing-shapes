b89cd43cd1afc48124d7705d46a2770730ebbd44
smichajlidis <smichajlidis@gmail.com>
2023-12-28 15:31:20 +0100

    delete setShape function prototype and set pos and color values as constructor args

--- include/Circle.hpp
+++ include/Circle.hpp
@@ -7,10 +7,9 @@ class Circle: public Figure {
 private:
     sf::CircleShape shape;
 public:
-    virtual void setShape(float pos_x, float pos_y, sf::Color color) override;
     virtual void draw(sf::RenderWindow& window) const override;
     virtual void move() override;
-    Circle(float);
+    Circle(float, float, float, sf::Color);
 };
 
 #endif
