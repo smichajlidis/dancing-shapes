f61a0a2b88dc77d64656d7a29778482943052b54
smichajlidis <smichajlidis@gmail.com>
2023-12-28 16:24:25 +0100

    add a dot end set each figure in the middle

--- main.cpp
+++ main.cpp
@@ -9,9 +9,10 @@ int main()
     sf::RenderWindow window(sf::VideoMode(800, 600), "Dancing shapes");
 
     std::vector<std::unique_ptr<Figure>> figures;
-    figures.push_back(std::make_unique<Circle>(100.0f, 25.0f, 150.0f, sf::Color::Green));
-    figures.push_back(std::make_unique<Rectangle>(140.0f,140.0f,55.0f,180.0f,sf::Color::Yellow));
-    figures.push_back(std::make_unique<Circle>(100.0f,3.0f,25.0f,150.0f,sf::Color::Red));
+    figures.push_back(std::make_unique<Circle>(100.0f, 300.0f, 200.0f, sf::Color::Green));
+    figures.push_back(std::make_unique<Rectangle>(140.0f,140.0f,330.0f,230.0f,sf::Color::Yellow));
+    figures.push_back(std::make_unique<Circle>(100.0f,3.0f,300.0f,200.0f,sf::Color::Red));
+    figures.push_back(std::make_unique<Circle>(5.0f, 395.0f, 295.0f, sf::Color::Blue));
 
     std::unique_ptr<Figure>& active = figures.at(0);
 
