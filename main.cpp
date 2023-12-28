2a3bc41da847537dfe92d85914cbdace627e935b
smichajlidis <smichajlidis@gmail.com>
2023-12-28 15:33:30 +0100

    delete setShape line

--- main.cpp
+++ main.cpp
@@ -8,10 +8,9 @@ int main()
     sf::RenderWindow window(sf::VideoMode(800, 600), "Dancing shapes");
 
     std::vector<std::unique_ptr<Figure>> figures;
-    figures.push_back(std::make_unique<Circle>(100.0f));
+    figures.push_back(std::make_unique<Circle>(100.0f, 25.0f, 150.0f, sf::Color::Green));
 
     std::unique_ptr<Figure>& active = figures.at(0);
-    active->setShape(25.0f, 150.0f, sf::Color::Green);
 
     while (window.isOpen())
     {
