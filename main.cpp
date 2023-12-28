6b280fa41084449d968584b7d52c2998fd9bc4f1
smichajlidis <smichajlidis@gmail.com>
2023-12-28 17:03:26 +0100

    change active figure by pressing c,d,r or t

--- main.cpp
+++ main.cpp
@@ -14,16 +14,31 @@ int main()
     figures.push_back(std::make_unique<Circle>(100.0f,3.0f,300.0f,200.0f,sf::Color::Red));
     figures.push_back(std::make_unique<Circle>(5.0f, 395.0f, 295.0f, sf::Color::Blue));
 
-    std::unique_ptr<Figure>& active = figures.at(0);
-
+    int activeFigureIndex {0};
+    
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed) {
                 window.close();
+            }
+	        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::C)) {
+   			    activeFigureIndex = 0;
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::R)) {
+   			    activeFigureIndex = 1;
+            }
+            if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::T)) {
+   			    activeFigureIndex = 2;  
+            }
+	        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
+   			    activeFigureIndex = 3;
+            }
         }
+
+        std::unique_ptr<Figure>& active = figures.at(activeFigureIndex);
 
         window.clear();
         active->move();
