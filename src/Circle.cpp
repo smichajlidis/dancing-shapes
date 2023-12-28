9e23d1e196713b6f0164b71346e29507cedd9898
smichajlidis <smichajlidis@gmail.com>
2023-12-28 15:32:44 +0100

    delete definition of setShape function and set color and position of an object in the constructor

--- src/Circle.cpp
+++ src/Circle.cpp
@@ -1,17 +1,13 @@
 #include "../include/Circle.hpp"
 
-void Circle::setShape(float pos_x, float pos_y, sf::Color color) {
-    shape.setPosition(pos_x, pos_y);
-    shape.setFillColor(color);
-    }
-
 void Circle::draw(sf::RenderWindow& window) const {
         window.draw(shape);
     }
 
-Circle::Circle(float value) {
- //   sf::CircleShape temp {100.0f};
+Circle::Circle(float value, float pos_x, float pos_y, sf::Color color) {
     shape = sf::CircleShape {value};
+    shape.setPosition(pos_x, pos_y);
+    shape.setFillColor(color);
 }
 
 void Circle::move() {
