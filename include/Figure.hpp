04a6703a59ddd178d265bcfd321ebf2b6389f74b
smichajlidis <smichajlidis@gmail.com>
2023-12-28 20:37:26 +0100

    define move, draw and rotate methods

--- include/Figure.hpp
+++ include/Figure.hpp
@@ -1,12 +1,32 @@
 #ifndef _FIGURE_HPP_
 #define _FIGURE_HPP_
 #include <SFML/Graphics.hpp>
+#include <memory>
 
 class Figure {
+protected:
+	std::unique_ptr<sf::Shape> shape;
 public:
-    virtual void draw(sf::RenderWindow& window) const = 0;
-    virtual void move() = 0;
-	virtual void rotate() = 0;
+    virtual void draw(sf::RenderWindow& window) const {
+		window.draw(*shape);
+	};
+    virtual void move() {
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) 
+   			 shape->setPosition(shape->getPosition().x - 1.f, shape->getPosition().y);	    
+	    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
+   			shape->setPosition(shape->getPosition().x + 1.f, shape->getPosition().y);
+	    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
+   			 shape->setPosition(shape->getPosition().x, shape->getPosition().y-1.f);	       
+	    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
+   			 shape->setPosition(shape->getPosition().x, shape->getPosition().y+1.f);	
+	};
+	virtual void rotate() {
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Q)) 
+   			 shape->setRotation(shape->getRotation() - 1);	    
+	    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::E))
+   			shape->setRotation(shape->getRotation() + 1);
+	};
+	Figure(std::unique_ptr<sf::Shape> shape) : shape(std::move(shape)) {}
     virtual ~Figure() {};
 };
 
