1df16e373cfc08dd1e0e4f2702e246b678346186
smichajlidis <smichajlidis@gmail.com>
2023-12-29 15:48:15 +0100

    delete pos values from each shapes initializations

--- main.cpp
+++ main.cpp
@@ -9,10 +9,10 @@ int main()
     sf::RenderWindow window(sf::VideoMode(800, 600), "Dancing shapes");
 
     std::vector<std::unique_ptr<Figure>> figures;
-    figures.push_back(std::make_unique<Circle>(100.0f, 300.0f, 200.0f, sf::Color::Green));
-    figures.push_back(std::make_unique<Rectangle>(140.0f,140.0f,330.0f,230.0f,sf::Color::Yellow));
-    figures.push_back(std::make_unique<Circle>(100.0f,3.0f,300.0f,200.0f,sf::Color::Red));
-    figures.push_back(std::make_unique<Circle>(5.0f, 395.0f, 295.0f, sf::Color::Blue));
+    figures.push_back(std::make_unique<Circle>(100.0f, sf::Color::Green));
+    figures.push_back(std::make_unique<Rectangle>(140.0f, 140.0f, sf::Color::Yellow));
+    figures.push_back(std::make_unique<Circle>(100.0f, 3.0f, sf::Color::Red));
+    figures.push_back(std::make_unique<Circle>(5.0f, sf::Color::Blue));
 
     int activeFigureIndex {0};
     
