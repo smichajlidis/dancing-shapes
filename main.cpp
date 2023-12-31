f9a7c33f908f005f2ac525367ac9f1ead1294e84
smichajlidis <smichajlidis@gmail.com>
2023-12-31 15:18:32 +0100

    add swapFigures(event) method to move figures in figures vector

--- main.cpp
+++ main.cpp
@@ -10,23 +10,25 @@ int main()
     sf::RenderWindow window(sf::VideoMode(800, 600), "Dancing shapes");
     FigureManager figures;
 
-    figures.addFigure(std::make_shared<Circle>(100.0f, sf::Color::Green));
-    figures.addFigure(std::make_shared<Rectangle>(140.0f, 140.0f, sf::Color::Yellow));
-    figures.addFigure(std::make_shared<Circle>(100.0f, 3.0f, sf::Color::Red));
-    figures.addFigure(std::make_shared<Circle>(5.0f, sf::Color::Blue));
+    figures.addFigure(std::make_shared<Circle>(100.0f, sf::Color::Green), "circle");
+    figures.addFigure(std::make_shared<Rectangle>(140.0f, 140.0f, sf::Color::Yellow), "rectangle");
+    figures.addFigure(std::make_shared<Circle>(100.0f, 3.0f, sf::Color::Red), "triangle");
+    figures.addFigure(std::make_shared<Circle>(5.0f, sf::Color::Blue), "point");
     
     while (window.isOpen())
     {
         sf::Event event;
-        while (window.pollEvent(event))
-        {
+        while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
-            figures.setActiveFigureIndex();
+            if (event.type == sf::Event::KeyPressed) {
+                figures.swapFigures(event);
+            }
+            figures.setActiveFigureName();
         }
-
-        figures.update();
+        figures.update(event);
+        window.clear();
         figures.draw(window);
     }
 
