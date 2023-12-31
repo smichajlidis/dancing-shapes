80fd747afe1f0472f715720749d407a6fff41689
smichajlidis <smichajlidis@gmail.com>
2023-12-31 19:05:36 +0100

    correct curly braces

--- main.cpp
+++ main.cpp
@@ -5,8 +5,7 @@
 #include <memory>
 #include <vector>
 
-int main()
-{
+int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Active figure: CIRCLE");
     FigureManager figures;
 
@@ -16,8 +15,7 @@ int main()
     figures.addFigure(std::make_shared<Circle>(100.0f, 3.0f, sf::Color::Red), "triangle");
     figures.addFigure(std::make_shared<Circle>(5.0f, sf::Color::Blue), "point");
     
-    while (window.isOpen())
-    {
+    while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
