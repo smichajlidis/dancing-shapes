d7c4462329ebca7d6e6732e3b4f58f4ce75987e3
smichajlidis <smichajlidis@gmail.com>
2024-01-02 18:09:46 +0100

    move non pure virtual methods into Figure.cpp

--- include/Figure.hpp
+++ include/Figure.hpp
@@ -10,36 +10,15 @@ protected:
 
 public:
     // Virtual function to draw the figure on a given window
-    virtual void draw(sf::RenderWindow& window) const {
-        window.draw(*shape);
-    };
-
+    virtual void draw(sf::RenderWindow& window) const;
     // Virtual function to move the figure based on keyboard input
-    virtual void move() {
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left))
-            shape->setPosition(shape->getPosition().x - 1.f, shape->getPosition().y);
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right))
-            shape->setPosition(shape->getPosition().x + 1.f, shape->getPosition().y);
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Up))
-            shape->setPosition(shape->getPosition().x, shape->getPosition().y - 1.f);
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Down))
-            shape->setPosition(shape->getPosition().x, shape->getPosition().y + 1.f);
-    };
-
+    virtual void move();
     // Virtual function to rotate the figure based on keyboard input
-    virtual void rotate() {
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Q))
-            shape->rotate(-1.0f);
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::E))
-            shape->rotate(1.0f);
-    };
-
+    virtual void rotate();
     // Pure virtual function to change the size of the figure (to be implemented by derived classes)
     virtual void changeSize() = 0;
-
     // Constructor accepting a unique pointer to an sf::Shape
     Figure(std::unique_ptr<sf::Shape> shape) : shape(std::move(shape)) {};
-
     // Virtual destructor
     virtual ~Figure() {};
 };
